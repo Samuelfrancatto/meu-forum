0a8cd056173f6c3307c02159f78ac8bfee702851
Samuel Francatto <87045945+Samuelfrancatto@users.noreply.github.com>
2024-10-03 22:09:19 -0300

    criado sistema para criar threads (não estilizado)

---
+++ backend/create-thread.php
@@ -0,0 +1,50 @@
+
+
+
+<?php 
+
+include "connect.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    $title = $_POST['title'] ?? '';
+    $content = $_POST['content'] ?? '';
+
+    $is_nsfw = isset($_POST['is_nsfw']) ? 1 : 0;
+
+    $category_id = $_POST['category_id'] ??'';
+
+
+    if (empty($title) || empty($content)) {
+        echo 'Título e conteúdo são obrigatórios';
+        exit;
+    }
+
+    try{
+        $query = "INSERT INTO posts (title, content, is_nsfw, created_at) VALUES (:title, :content, :is_nsfw, NOW())";
+
+        $stmt = $db->prepare("$query");
+
+        $stmt->bindParam(':title', $title);
+
+        $stmt->bindParam(':content', $content);
+
+        $stmt->bindParam(':is_nsfw', $is_nsfw, PDO::PARAM_INT);
+
+        $stmt->bindParam(':category_id', $category_id, PDO::PARAM_INT);
+
+        if ($stmt->execute()) {
+            echo 'Post criado com sucesso!';
+        } else {
+            echo 'Erro ao criar o post.';
+        }
+
+    } catch(PDOException $e){
+        echo 'Erro no banco de dados: ' . $e->getMessage();
+    }
+
+} else{
+    echo 'Método de envio inválido';
+}
+
+?>
