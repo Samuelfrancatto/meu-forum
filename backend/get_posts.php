376e8a228ae41d464a22b39f7c1aab5cb5c48dc9
Samuel Francatto <87045945+Samuelfrancatto@users.noreply.github.com>
2024-10-09 15:07:39 -0300

    terminado sistema de respostas (com estilização)

--- backend/get_posts.php
+++ backend/get_posts.php
@@ -7,7 +7,7 @@ $category_id = isset($_GET["category_id"]) ? intval($_GET["category_id"]) : 0;
 
 if ($category_id > 0) {
     // Consulta para obter todos os posts da categoria
-    $query = "SELECT * FROM posts WHERE category_id = :category_id";
+    $query = "SELECT * FROM posts WHERE category_id = :category_id AND parent_id IS NULL";
     $stmt = $db->prepare($query);
     $stmt->bindParam(":category_id", $category_id, PDO::PARAM_INT);
     $stmt->execute();
--- backend/get_replies.php
+++ backend/get_replies.php
@@ -4,6 +4,32 @@
 
 include "connect.php";
 
+$parent_id = $_GET['parent_id'] ?? '';
 
+if ($parent_id){
+
+    try{
+        $query = "SELECT * FROM posts WHERE parent_id = :parent_id";
+
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(":parent_id", $parent_id, PDO::PARAM_INT);
+
+        if ( $stmt->execute() ){
+            $responses = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else{
+            echo 'Falha na execução da consulta.';
+        }
+    
+        
+    
+        header('Content-Type: application/json');
+        echo json_encode($responses);
+    } catch(PDOException $e){
+        echo json_encode(['error'=> 'Erro no banco de dados: ' . $e->getMessage()]);
+    }
+   
+}else{
+    echo json_encode(['error'=> 'ID do post principal não fornecido.']);
+}
 
 ?>
